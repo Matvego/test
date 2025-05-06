5c17b9f47f4dd3021cce1a1b81c286cf46831bed
Matvego <matvejslenkin436@gmail.com>
2025-05-06 10:20:22 +0400

    Add files via upload

---
+++ Шлёнкин Матвей/тест/main.cpp
@@ -0,0 +1,94 @@
+//========================================================================
+//! @file       Main.cpp
+//{=======================================================================
+//!
+//! @brief      <Заголовок>\n
+//! @brief      <Подзаголовок>
+//!
+//! @version    [Version 0.01 alpha, build 1]
+//! @author     Copyright (C) <Автор>, <Год> (<Имя> <Почта>)
+//! @date       <Дата>
+//!
+//! @par        Протестировано
+//!           - (TODO: список платформ)
+//!
+//! @todo     - (TODO: список ближайших планов по этому файлу)
+//!
+//! @bug      - (TODO: список найденных ошибок в этом файле)
+//!
+//! @par        История изменений файла
+//!           - Версия 0.01 Alpha
+//!             - Только что созданный файл
+//!
+//}=======================================================================
+
+#include "TXLib.h"
+void drawMaket()
+{
+
+    txSetColor(TX_WHITE);
+    txSetFillColor(TX_TRANSPARENT);
+    txRectangle(50,20,925,205);
+
+    txSetFillColor(TX_TRANSPARENT);
+    txRectangle(10,10,990,690);
+
+    txRectangle(30,250,310,550);
+    txRectangle(350,250,620,550);
+    txRectangle(650,250,940,550);
+
+    txRectangle(30,560,310,630);
+    txRectangle(350,560,620,630);
+    txRectangle(650,560,940,630);
+
+}
+
+
+int main()
+{
+txCreateWindow (1000, 700);
+txTextCursor (false);
+    while (!GetAsyncKeyState(VK_ESCAPE))
+    {
+    drawMaket();
+    txSetColor (TX_WHITE);
+    txSelectFont ("Comic Sans", 60);
+    txSetTextAlign (TA_CENTER);
+    txTextOut (490,80, "1.Столица России");
+    txDrawText(30,560,310,630,"Амстердам");
+    txDrawText(350,560,620,630,"Челябинск");
+    txDrawText(650,560,930,630,"Москва");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+txDisableAutoPause();
+return 0;
+}
+
